00f2b6923a699c32ea0d51370b098e2de86ae536
myrza <Gapusta97@gmail.com>
2020-10-11 22:12:50 +0600

    Added '+' binary operation support

--- src/main/java/edu/myrza/App.java
+++ src/main/java/edu/myrza/App.java
@@ -1,7 +1,26 @@
 package edu.myrza;
 
+import java.io.PrintStream;
+
 public class App {
+    
+    static PrintStream out = System.out;
+	
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        
+	if(args.length != 3) {
+		out.println("Correct usage [ $ java <prog-name> <op> <operand1> <operand2> ]");
+		return;
+	}
+
+	String op = args[0];
+	double numOne = Double.parseDouble(args[1]);
+	double numTwo = Double.parseDouble(args[2]);
+
+	switch(op) {
+		case "+" : out.println(numOne + numTwo); break;
+		default : out.println("No such operation : " + op);
+	}
+
     }
 }
